312259c4e41e55dcc101036fbfcdf35d8878038d
hongggweiii <103654486+13ack2basixs@users.noreply.github.com>
2026-03-21 00:33:57 +0800

    Fix incorrect return type for Trade and extract Reader initialisation out

--- internal/broker/consumer.go
+++ internal/broker/consumer.go
@@ -14,27 +14,31 @@ type KafkaConsumer struct {
 }
 
 func NewKafkaConsumer(brokerAddress string, topic string, groupId string) *KafkaConsumer {
+	// Call factory function of kafka.Reader
+	r := kafka.NewReader(kafka.ReaderConfig{
+		Brokers: []string{brokerAddress},
+		Topic:   topic,
+		GroupID: groupId,
+	})
+
 	return &KafkaConsumer{
-		reader: &kafka.Reader(kafka.ReaderConfig{
-			Brokers: []string{brokerAddress},
-			Topic:   topic,
-			GroupID: groupId,
-		}),
+		reader: r,
 	}
 }
 
+// Consume trade message, unmarshal and return them
 func (c *KafkaConsumer) ConsumeTrade(ctx context.Context) (domain.Trade, error) {
 	trade := new(domain.Trade)
 
 	msg, err := c.reader.ReadMessage(ctx)
 	if err != nil {
-		return nil, err
+		return domain.Trade{}, err
 	}
 
 	err = json.Unmarshal(msg.Value, trade)
 	if err != nil {
 		fmt.Printf("Error while unserializing: %v", err)
-		return nil, err
+		return domain.Trade{}, err
 	}
 
 	return *trade, nil
